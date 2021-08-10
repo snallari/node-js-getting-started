c5bb64a037420e7a68add02f965d215a17460c8a
nallari <shruti.nallari@capgemini.com>
2021-08-10 13:38:57 -0400

    cors

--- serversai.js
+++ serversai.js
@@ -8,7 +8,7 @@ var MongoClient = mongodb.MongoClient
 app.use(cors())
 app.options('*', cors())
 
-app.get('/listClasses',  function (req, respo) {
+app.get('/listClasses', cors(),  function (req, respo) {
     console.log("its inside")
     var url = 'mongodb+srv://snallari:Sairam90@cluster0.iqgwh.mongodb.net/test'
     MongoClient.connect(url, function (err, client) {
@@ -39,7 +39,7 @@ app.get('/listClasses',  function (req, respo) {
     // });
 });
 
-app.post('/addClasses',  function (req, response) {
+app.post('/addClasses', cors(), function (req, response) {
     console.log("its inside")
     var url ='mongodb+srv://snallari:Sairam90@cluster0.iqgwh.mongodb.net/test'
     // First read existing users.
